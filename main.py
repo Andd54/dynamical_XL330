86b654a4f27eac42638252ae4036cbe46f0c713f
Ziyi_Wang <anderprima54@gmail.com>
2024-02-22 21:47:09 +0000

    servo information read

--- main.py
+++ main.py
@@ -19,8 +19,13 @@ else:
 
 os.sys.path.append("./DynamixelSDK/python/src")  # Path setting
 
-import dynamixel_sdk as dynamixel # Uses Dynamixel SDK library
-print(dir(dynamixel))
+import dynamixel_sdk 
+from dynamixel.driver import (
+    DynamixelDriver,
+    DynamixelDriverProtocol,
+    FakeDynamixelDriver
+)
+# Uses Dynamixel SDK library
 # Model XL330-M288
 
 DXL_ID = 1
@@ -45,10 +50,29 @@ COMM_TX_FAIL                = -1001                         # Communication Tx F
 # Initialize PortHandler Structs
 # Set the port path
 # Get methods and members of PortHandlerLinux or PortHandlerWindows
-from dynamixel.driver import (
-    DynamixelDriver,
-    DynamixelDriverProtocol,
-    FakeDynamixelDriver,
-)
+protocol:DynamixelDriverProtocol
 
-driver : DynamixelDriverProtocol
+try:
+    servo_1 = DynamixelDriver([1])
+    servo_2 = DynamixelDriver([2])
+    servo_3 = DynamixelDriver([3])
+except FileNotFoundError:
+    servo_1 = DynamixelDriver([1], port="/dev/ttyUSB0")
+    servo_2 = DynamixelDriver([2], port="/dev/ttyUSB0")
+    servo_3 = DynamixelDriver([3], port="/dev/ttyUSB0")
+
+    servo_1.set_torque_mode(True)
+    servo_1.set_torque_mode(False)
+    servo_2.set_torque_mode(True)
+    servo_2.set_torque_mode(False)
+    servo_3.set_torque_mode(True)
+    servo_3.set_torque_mode(False)
+try:
+        while True:
+            joint_angles_1 = servo_1.get_joints()
+            joint_angles_2 = servo_2.get_joints()
+            joint_angles_3 = servo_3.get_joints()
+            # print(f"Joint angles for IDs {ids}: {joint_angles}")
+            # print(f"Joint angles for IDs {ids[1]}: {joint_angles[1]}")
+except KeyboardInterrupt:
+        servo_2.close()
