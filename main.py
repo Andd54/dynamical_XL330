4bdcc8eeb11a36bbfe5d85936a45cbfb7d9ebc57
Ziyi_Wang <anderprima54@gmail.com>
2024-02-22 20:39:34 +0000

    drivers added for testing

--- main.py
+++ main.py
@@ -17,6 +17,38 @@ else:
         return sys.stdin.read(1)
 
 
-os.sys.path.append("./DynamixelSDK")  # Path setting
+os.sys.path.append("./DynamixelSDK/python/src")  # Path setting
 
-import dynamixel_sdk as dynamixel  # Uses Dynamixel SDK library
+import dynamixel_sdk as dynamixel # Uses Dynamixel SDK library
+print(dir(dynamixel))
+# Model XL330-M288
+
+DXL_ID = 1
+BAUDRATE                    = 57600
+DEVICENAME                  = "/dev/ttyUSB0".encode('utf-8')
+
+PROTOCOL_VERSION = 2
+
+ADDR_PRO_PRESENT_POSITION    = 132
+
+TORQUE_ENABLE               = 1                             # Value for enabling the torque
+TORQUE_DISABLE              = 0                             # Value for disabling the torque
+DXL_MINIMUM_POSITION_VALUE  = -150000                       # Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 150000                        # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MOVING_STATUS_THRESHOLD = 20                            # Dynamixel moving status threshold
+
+ESC_ASCII_VALUE             = 0x1b
+
+COMM_SUCCESS                = 0                             # Communication Success result value
+COMM_TX_FAIL                = -1001                         # Communication Tx Failed
+
+# Initialize PortHandler Structs
+# Set the port path
+# Get methods and members of PortHandlerLinux or PortHandlerWindows
+from dynamixel.driver import (
+    DynamixelDriver,
+    DynamixelDriverProtocol,
+    FakeDynamixelDriver,
+)
+
+driver : DynamixelDriverProtocol
--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
 git clone git@github.com:ROBOTIS-GIT/DynamixelSDK.git
+python3 DynamixelSDK/python/setup.py install
